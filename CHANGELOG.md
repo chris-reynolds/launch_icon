9f88373cba7f008e3e34be5aefef0442fdb4e870
Chris Reynolds <chrisr@instantobjects.com>
2020-08-07 17:12:28 +1200

    Add api doc, license and a couple of tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-## 1.0.0
+## 0.0.1
 
 - Initial version, created by Stagehand
+
+## 0.1.0
+
+- Initial Version, hand-tested for publication
+
--- test/launch_icon_test.dart
+++ test/launch_icon_test.dart
@@ -1,9 +1,19 @@
+import 'dart:io';
+
 import 'package:launch_icon/launch_icon.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('load 2 sections', () {
+void main() async {
+  test('load 2 sections from yaml file', () {
     var sections = loadAndValidatePubspecYaml();
     expect(sections.length, 2);
+  });
+  test('can load png',() {
+     var img = loadSourceImage('test/assets/album_icon.jpg');
+     expect(img.length,512*512);
+  });
+  test('can load png',() {
+    var img = loadSourceImage('test/assets/album_icon2.png');
+    expect(img.length,512*491);
   });
 }
